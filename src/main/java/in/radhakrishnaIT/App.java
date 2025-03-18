3bd1f6fdc7483a69d616a0c9de5a1d99b3d3a65e
pradip <pp638607@gmail.com>
2025-03-18 23:43:54 +0530

    maven files are added

---
+++ src/main/java/in/radhakrishnaIT/App.java
@@ -0,0 +1,13 @@
+package in.radhakrishnaIT;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
